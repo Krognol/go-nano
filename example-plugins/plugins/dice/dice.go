fc8a33721395ac82b3c6f716239b3777dcaa9ac0
Krognol <timgabe@gmail.com>
2016-10-08 17:14:07 +0200

    fixed seeding in example plugin

--- example-plugins/plugins/dice/dice.go
+++ example-plugins/plugins/dice/dice.go
@@ -3,13 +3,16 @@ package dice
 import (
 	"math/rand"
 	"strconv"
+	"time"
 )
 
 func D6() int {
+	rand.Seed(time.Now().UnixNano())
 	return rand.Intn(6)
 }
 
 func Roll(n string) int {
+	rand.Seed(time.Now().UnixNano())
 	i, _ := strconv.Atoi(n)
 	return rand.Intn(i)
 }
